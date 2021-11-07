db17e5a2b78a9f605253f17ef10962107ea2b97b
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2021-11-07 05:43:08 +0000

    Created Circular models that utilise convolutions

---
+++ Circular/ConvFFCircular.py
@@ -0,0 +1,167 @@
+"""
+Uses the pre-training circular model to train a single-layer perceptron for classifying MNIST digits.
+"""
+
+import torch.nn as nn
+import torch
+import torch.optim as optim
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader, random_split
+import time
+import torch.optim.lr_scheduler as lr_s
+from ConvNewCircular import ConvNewCircular
+
+class ConvFFCircular(nn.Module):
+    def __init__(self):
+        super(ConvFFCircular, self).__init__()
+        self.fc4 = nn.Linear(2500, 10)
+        self.softmax = nn.Softmax(dim = 1)
+        self.gelu = nn.GELU()
+
+        model = ConvNewCircular()
+        model.load_state_dict(torch.load('new-circular.pickle'))
+        model.eval()
+        self.fc1 = model.fc1
+        self.fc2 = model.fc2
+        self.fc3 = model.fc3
+        self.bn1 = model.bn1
+        self.bn2 = model.bn2
+        self.bn3 = model.bn3
+        self.conv1 = model.conv1
+        self.conv2 = model.conv2
+        
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.gelu(x)
+        x = self.conv2(x)
+        x = self.gelu(x)
+
+        x = x.reshape(x.shape[0], -1)
+        
+        # These are the pre-trained layers which are not updated during the training of this model
+        x = self.fc1(x)
+        x = self.gelu(x)
+        x = self.bn1(x)
+
+        x = self.fc2(x)
+        x = self.gelu(x)
+        x = self.bn2(x)
+
+        x = self.fc3(x)
+        x = self.gelu(x)
+        x = self.bn3(x)
+
+        # New classification head that is trained in this model
+        x = self.fc4(x)
+        x = self.softmax(x)
+
+        return x
+
+start_time = time.time()
+
+num_epochs = 20
+device = torch.device('cpu')
+
+# Loads the train and test data into PyTorch tensors
+training_data = datasets.MNIST(root = "data", train = True, download = True, transform = ToTensor())
+test_data = datasets.MNIST(root = "data", train = False, download = True, transform = ToTensor())
+training_data, validation_set = random_split(training_data,[50000,10000])
+
+# Loads the data into batches 
+train_dataloader = DataLoader(training_data, batch_size = 200, shuffle = True)
+valid_dataloader = DataLoader(validation_set, batch_size = 200, shuffle = True)
+test_dataloader = DataLoader(test_data, batch_size = 200, shuffle = True)
+
+
+model = ConvFFCircular().to(device)
+
+# Prevents additional training or pre-trained layers
+for param in model.parameters():
+    if param.shape == torch.Size([10, 2500]) or param.shape == torch.Size([10]):
+        pass
+    else:
+        param.requires_grad = False
+
+loss_f = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.0001, weight_decay = 0)
+scheduler = lr_s.ReduceLROnPlateau(optimizer, 'min', patience = 2)
+
+old_loss = 10000
+times_worse = 0
+
+for epoch in range(num_epochs):
+    print(epoch)
+    train_loss = 0.0
+
+    for batch_idx, (data, labels) in enumerate(train_dataloader):
+        #print(batch_idx)
+        #print(model.conv1.weight)
+
+        data = data.to(device = device)
+        labels = labels.to(device = device)
+
+        scores = model(data) # Runs a forward pass of the model for all the data
+        loss = loss_f(scores, labels) # Calculates the loss of the forward pass using the loss function
+        train_loss += loss
+
+        optimizer.zero_grad() # Resets the optimizer gradients to zero for each batch
+        loss.backward() # Backpropagates the network using the loss to calculate the local gradients
+
+        optimizer.step() # Updates the network weights and biases
+
+
+    valid_loss = 0.0
+    model.eval()
+    for data, labels in valid_dataloader:
+        if torch.cuda.is_available():
+            data, labels = data.cuda(), labels.cuda()
+        
+        target = model(data)
+        loss = loss_f(target,labels)
+        valid_loss = loss.item() * data.size(0)
+
+    scheduler.step(valid_loss)
+    print('Validation Loss: ', valid_loss)
+
+    if valid_loss >= old_loss:
+        times_worse += 1
+    else:
+        times_worse = 0
+
+
+    if times_worse >= 3:
+        print('Reducing learning rate.')
+
+    if times_worse >= 5:
+        print('Stopping early.')
+        start_time = time.time()
+        break
+
+    old_loss = valid_loss
+
+
+# Checks the performance of the model on the test set
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device = device)
+            y = y.to(device = device)
+            #x = x.reshape(x.shape[0], -1)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+    print((num_correct * 100 / num_samples).item(), '%  Correct')
+
+print('\n')
+check_accuracy(test_dataloader, model)
+
+print('Finished in %s seconds' % round(time.time() - start_time, 1))
---
+++ Circular/ConvNewCircular.py
@@ -0,0 +1,151 @@
+"""
+Author: Sam Armstrong
+Date: November 2021
+
+Description: Implementation of the Circular neural network architecture that uses dense rather than sparse layers,
+and uses separate network layers for the full pass, rather than reversing the layers.
+"""
+
+import torch.nn as nn
+import torch
+import torch.optim as optim
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader, random_split
+import time
+import torch.optim.lr_scheduler as lr_s
+from Sparse import Sparse
+
+class ConvNewCircular(nn.Module):
+    def __init__(self):
+        super(ConvNewCircular, self).__init__()
+        self.sp = Sparse(784, 784, connections_per_neuron = 2)
+        self.fc = nn.Linear(784, 10)
+        self.gelu = nn.GELU()
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim = -1)
+        self.bn1 = nn.BatchNorm1d(1500)
+        self.bn2 = nn.BatchNorm1d(2500)
+        self.bn3 = nn.BatchNorm1d(2500)
+        self.bn4 = nn.BatchNorm1d(1500)
+
+        self.dropout = nn.Dropout(p = 0.5)
+
+        self.fc1 = nn.Linear(4704, 1500)
+        self.fc2 = nn.Linear(1500, 2500)
+        self.fc3 = nn.Linear(2500, 2500)
+        self.fc4 = nn.Linear(2500, 1500)
+        self.fc5 = nn.Linear(1500, 784)
+        self.conv1 = nn.Conv2d(1, 3, kernel_size = 3, padding = 1)
+        self.conv2 = nn.Conv2d(3, 6, kernel_size = 3, padding = 1)
+        self.conv3 = nn.Conv2d(1, 6, kernel_size = 3, padding = 1)
+        self.conv4 = nn.Conv2d(6, 1, kernel_size = 1, padding = 0)
+
+    def forward(self, x):
+        #x = x.reshape(x.shape[0], -1) # Flattens data
+
+        # Drops out an amount of the input
+        x = self.dropout(x)
+
+        # Propagating forwards into the network
+        x = self.conv1(x)
+        x = self.gelu(x)
+
+        x = self.conv2(x)
+        x = self.gelu(x)
+
+        x = x.reshape(x.shape[0], -1)
+
+        x = self.fc1(x)
+        x = self.gelu(x)
+        x = self.bn1(x)
+
+        x = self.fc2(x)
+        x = self.gelu(x)
+        x = self.bn2(x)
+
+        # Self-comparison layer (central layer training with relation to itself)
+        x = self.fc3(x)
+        x = self.gelu(x)
+        x = self.bn3(x)
+        
+        # Neuron gradients then propagate back through the network to the input layer
+        x = self.fc4(x)
+        x = self.gelu(x)
+        x = self.bn4(x)
+
+        x = self.fc5(x)
+        x = self.gelu(x)
+
+        x = x.reshape(x.shape[0], 1, 28, 28)
+
+        x = self.conv3(x)
+        x = self.gelu(x)
+
+        x = self.conv4(x)
+        x = self.relu(x) # Last activation is relu to better approximate the input values
+
+        x = x.reshape(x.shape[0], -1)
+
+        return x
+
+
+def train_model():
+    start_time = time.time()
+
+    num_epochs = 10
+    device = torch.device('cpu')
+
+    # Loads the train and test data into PyTorch tensors
+    training_data = datasets.MNIST(root = "data", train = True, download = True, transform = ToTensor())
+    test_data = datasets.MNIST(root = "data", train = False, download = True, transform = ToTensor())
+    training_data, validation_set = random_split(training_data,[50000,10000])
+
+    # Loads the data into batches
+    train_dataloader = DataLoader(training_data, batch_size = 200, shuffle = True)
+    valid_dataloader = DataLoader(validation_set, batch_size = 200, shuffle = True)
+    test_dataloader = DataLoader(test_data, batch_size = 200, shuffle = True)
+
+    model = ConvNewCircular().to(device)
+
+    params = []
+    params += model.parameters()
+
+    loss_f = nn.MSELoss()
+    optimizer = optim.Adam(params, lr = 0.0001, weight_decay = 0)
+    scheduler = lr_s.ReduceLROnPlateau(optimizer, 'min', patience = 2)
+
+    old_loss = 10000
+    times_worse = 0
+
+    for epoch in range(num_epochs):
+        print('Epoch: ', epoch)
+        train_loss = 0.0
+
+        for batch_idx, (data, labels) in enumerate(train_dataloader):
+            #print(batch_idx)
+
+            batch_size = data.shape[0]
+            data = data.to(device = device)
+            flattened_data = data.reshape(batch_size, 784).clone().detach()
+
+            scores = model(data) # Runs a forward pass of the model for all the data
+            loss = loss_f(scores, flattened_data) # Calculates the loss of the forward pass using the loss function
+            #print('Training Loss: ', loss.item())
+            train_loss += loss.item()
+
+            optimizer.zero_grad() # Resets the optimizer gradients to zero for each batch
+            loss.backward() # Backpropagates the network using the loss to calculate the local gradients
+
+            optimizer.step() # Updates the network weights and biases
+
+        print('Training Loss: ', train_loss)
+
+
+    torch.save(model.state_dict(), 'new-circular.pickle')
+    print('Circular saved to .pickle file')
+    print('Finished in %s seconds' % round(time.time() - start_time, 1))
+
+
+if __name__ == '__main__':
+    train_model()
